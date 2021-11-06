04af04097986307c54462dc426a598e461450ccf
Dovtutis <dovtutis@gmail.com>
2021-11-06 13:44:50 +0200

    pages boilerplate added

---
+++ src/pages/404.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../components/Layout";
+
+const Error = () => {
+  return (
+    <Layout>
+      <h1>Error page</h1>
+    </Layout>
+  );
+};
+
+export default Error;
---
+++ src/pages/tags.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../components/Layout";
+
+const Tags = () => {
+  return (
+    <Layout>
+      <h1>tags page</h1>
+    </Layout>
+  );
+};
+
+export default Tags;
