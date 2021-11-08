43eb93974033fdf8bf3d128ab67e55bdccb66107
Dovtutis <dovtutis@gmail.com>
2021-11-08 22:43:39 +0200

    additional packages and images added

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,12 +4,42 @@
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
 
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
 module.exports = {
   /* Your site config here */
+  siteMetadata: {
+    title: "Simply Recipes",
+    description: "Nice and clean recipes site",
+    author: "@dovtutis",
+    person: { name: "Dovydas", age: 29 },
+    simpleData: ["item 1", "item 2"],
+    complexData: [
+      { name: "Dovydas", age: 29 },
+      { name: "Ziuzan", age: 26 },
+    ],
+  },
   plugins: [
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/assets/images`,
+      },
+    },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `fvxny3gfyy18`,
+        // Learn about environment variables: https://gatsby.dev/env-vars
+        accessToken: process.env.CONTENTFUL_API_KEY,
+      },
+    },
   ],
 };
